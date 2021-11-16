c46d473d7bb6d8b9abcd3b7f33e737d53a8f483a
Liam Eller <liamthelastson@gmail.com>
2021-11-16 09:59:07 -0800

    Add random functionality to application.
    Co-authored-by: Curtis Brooks <curtisbrooks678@gmail.com>

--- src/index.js
+++ src/index.js
@@ -13,19 +13,24 @@ $(document).ready(function () {
     let request = new XMLHttpRequest();
     const url = `https://api.giphy.com/v1/gifs/${type}?api_key=${process.env.API_KEY}&q=${key}&limit=25&offset=0&lang=en`;
 
+    console.log(url);
     request.onreadystatechange = function () {
       if (this.readyState === 4 && this.status === 200) {
         const response = JSON.parse(this.responseText);
         getElements(response);
       }
     };
-
+    
     request.open("GET", url, true);
     request.send();
-
+    
     function getElements(response) {
-      for (let i = 0; i < response.data.length; i++) {
-        $('#display').append("<img src ='" + response.data[i].images.original.url + "'/>");
+      if (type === "random") {
+        $('#display').append("<img src ='" + response.data.images.original.url + "'/>");
+      } else {
+        for (let i = 0; i < response.data.length; i++) {
+          $('#display').append("<img src ='" + response.data[i].images.original.url + "'/>");
+        }
       }
     }
   });
